45cf0335d9df6d697263fb02d284b8be61b00e14
celsobritodev <celsinhobrito@gmail.com>
2024-05-12 22:16:59 -0300

    salvando no banco de dados

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,19 +1,23 @@
 package controller;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.JavaBeans;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main","/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 
 	public Controller() {
 		super();
@@ -23,16 +27,48 @@ public class Controller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String action = request.getServletPath();
-		System.out.println(action);
+	//	System.out.println(action);
 		if (action.equals("/main")) {
 			contatos(request, response);
+		} else if (action.equals("/insert")) {
+			novoContato(request,response);
+		} else {
+			response.sendRedirect("index.html");
 		}
 	}
 
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.sendRedirect("agenda.jsp");
+		ArrayList<JavaBeans> lista = dao.listarContatos();
+		request.setAttribute("contatos", lista);
+	    RequestDispatcher rd = request.getRequestDispatcher("agenda.jsp");
+        rd.forward(request, response);	    
+/*		
+		for (int i=0; i<lista.size(); i++) {
+			System.out.println(lista.get(i).getIdcon());
+			System.out.println(lista.get(i).getNome());
+			System.out.println(lista.get(i).getFone());
+			System.out.println(lista.get(i).getEmail());
+				}
+*/			
+			
+	
 
 	}
+	
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	//	System.out.println(request.getParameter("nome"));
+	//	System.out.println(request.getParameter("fone"));
+	//	System.out.println(request.getParameter("email"));
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		dao.inserirContato(contato);
+		response.sendRedirect("main");
+		
+
+	}
+
 
 }
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -2,10 +2,12 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DAO {
- 
 
 	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	public static final String URL = "jdbc:mysql://localhost:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
@@ -13,9 +15,9 @@ public class DAO {
 	public static final String USER_PASSWD = "admin";
 
 	Connection con;
-	
+
 	public static Connection getConnection() {
-		
+
 		Connection con = null;
 		try {
 			Class.forName(DRIVER); // força o carregamento do driver
@@ -26,24 +28,49 @@ public class DAO {
 			System.out.println("Não pode conectar:" + e.getMessage());
 		} catch (ClassNotFoundException e) {
 			System.out.println("Driver não encontrado!");
-			
+
 		}
 		return con;
 	}
-	
-	
-/*	
-	public void testeConexao() {
+
+	public void inserirContato(JavaBeans contato) {
+		String create = "insert into contatos (nome,fone,email) values (?,?,?)";
 		try {
 			Connection con = getConnection();
-			System.out.println(con);
+			PreparedStatement pst = con.prepareStatement(create);
+			pst.setString(1, contato.getNome());
+			pst.setString(2, contato.getFone());
+			pst.setString(3, contato.getEmail());
+			pst.executeUpdate();
 			con.close();
+
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-*/
-	
-	
-	
+
+	public ArrayList<JavaBeans> listarContatos() {
+		ArrayList<JavaBeans> contatos = new ArrayList<>();
+		String read = "SELECT * FROM CONTATOS ORDER BY NOME";
+		try {
+			Connection con = getConnection();
+			PreparedStatement pst = con.prepareStatement(read);
+			ResultSet rs = pst.executeQuery();
+			while (rs.next()) {
+				String idcon = rs.getString(1);
+				String nome = rs.getString(2);
+				String fone = rs.getString(3);
+				String email = rs.getString(4);
+				contatos.add(new JavaBeans(idcon, nome, fone, email));
+
+			}
+			con.close();
+			return contatos;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+
+	}
+
 }
